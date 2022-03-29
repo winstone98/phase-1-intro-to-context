24f0507c0b2e5532ada0d1da4f44afc2a6de5f16
mckinseyleckenby <leckenbym11@gmail.com>
2022-03-29 18:25:54 -0400

    commit tree 1b055e386f71ec9a31ef5b5762481452ecf6b125

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function createEmployeeRecord(row ){
+    let obj = {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return obj
+}
+
+function createEmployeeRecords(array){
+    return array.map(createEmployeeRecord(row => row))
+}
